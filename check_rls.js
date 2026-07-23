3f696dc939aceabb3d0fba158c48d73823bdf7b6
Dwiky Sumarlin <dwikysumarlin01@gmail.com>
2026-07-23 15:33:15 +0700

    chore: clean up temporary scripts, debug dumps and update .gitignore

--- check_rls.js
+++
@@ -1,9 +0,0 @@
-const { Client } = require('pg');
-require('dotenv').config({ path: '.env.local' });
-const client = new Client({ connectionString: process.env.DATABASE_URL });
-(async () => {
-  await client.connect();
-  const { rows } = await client.query("SELECT tablename, rowsecurity FROM pg_tables WHERE schemaname = 'public'");
-  console.log(rows);
-  await client.end();
-})();
--- test_action.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config({ path: '.env.local' });
-const { getMachineBlockAnalytics } = require('./actions/dashboard-actions');
-
-async function test() {
-  const res = await getMachineBlockAnalytics("R1", 30);
-  console.log(JSON.stringify(res, null, 2));
-}
-
-test();
